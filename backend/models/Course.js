18f692bea868c81b648ac4f610c194831d3b4c83
vibhor <vibhorgupta00ta@gmail.com>
2026-06-18 19:40:39 +0530

    Course Model

---
+++ backend/models/Course.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const courseSchema=new mongoose.Schema({
+    title:{
+        required:true,
+        type:String,
+    },
+    description:{
+        required:true,
+        type:String,
+    },
+    price:{
+        required:true,
+        type:Number,
+    },
+    category:{
+        required:true,
+        type:String,
+    },
+    thumbnail:{
+        required:true,
+        type:String,
+    },
+    instructor:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+    },
+    lessons:[{
+        title:{
+            required:true,
+            type:String,
+        },
+        youtubeUrl:{
+            required:true,
+            type:String,
+        }
+    }]
+},{
+    timestamps:true,
+}
+)
+
+const Course=mongoose.model('Course',courseSchema);
+export default Course;
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,15 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
+
+router.get("/profile", authMiddleware, (req, res) => {
+  res.json({
+    message: "Profile Accessed",
+    user: req.user,
+  });
+});
 export default router;
